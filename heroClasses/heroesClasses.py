0bd72d276fb07ae09ad747f6103fb06dcc165029
peter <petermy666@gmail.com>
2024-09-26 13:35:53 +0400

    ...

--- heroClasses/heroesClasses.py
+++ heroClasses/heroesClasses.py
@@ -34,12 +34,15 @@ class Hero:
     def get_new_level(self):
         if self.get_exp() >= 1000:
             self.__level = 3
+            self.add_skill()
         elif self.get_exp() >= 500:
             self.__level = 2
+            self.add_skill()
         elif self.get_exp() >= 200:
             self.__level = 1
+            self.add_skill()
         else:
-            self.level = 0
+            self.__level = 0
         return f"Герой {self.get_name()}, теперь {self.get_level()} уровня, навыки: {', '.join(self.my_hero_skills)}"
 
     def add_exp(self, exp):
@@ -47,13 +50,25 @@ class Hero:
         new_level = self.get_new_level()
         return new_level
 
+    def add_skill(self):
+        pass
+
 class MyHero(Hero):
 
     def __init__(self, name, character_class):
         super().__init__(name)
+        self.__character_class = character_class
         self.__skill_list = super().get_skills(character_class)
 
     def get_skill_list(self):
         return self.__skill_list
 
-
+    def add_skill(self):
+        while True:
+            skill = input(f'{super().get_name()}, выберете навык: {', '.join(self.get_skill_list())}')
+            while skill not in self.get_skill_list():
+                skill = input(f'НЕВЕРНЫЙ ВЫБОР!! Выберете навык: {', '.join(self.get_skill_list())}')
+            super().my_hero_skills.append(skill)
+            self.__skill_list.remove(skill)
+            if  list(self.get_skill_list()) == super().get_level():
+                break
